920cdc2edde0187c8a3d6b6124445f0d4643060a
hemalatha <hemalathabora9@gmail.com>
2025-04-03 19:51:32 +0530

    Updated ALLOWED_HOSTS and configured for deployment

--- disaster_management/disaster_management/settings.py
+++ disaster_management/disaster_management/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-s-bk3%(gdtd&g_5y(db1_oz5-1w(7334twx%_cnm6_3b)wau1@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", "localhost",'disaster_app.onrender.com']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -122,6 +123,8 @@ STATICFILES_DIRS = [
 ]
 
 
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
